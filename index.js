b5cedb4773e69caf100622a8f6c778b94a0efcd4
Kayla Phelps <kmphelps822@gmail.com>
2021-07-20 18:49:15 -0700

    commit tree a3db83760ff63fc6d8dd7c277bc266f48bcb981a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(n="*"){
+    return function(x="special") {
+        return `You are ${n}${x}${n}!`;
+    }
+        
+}
+wrapAdjective("%")("a hard worker")
+
+
